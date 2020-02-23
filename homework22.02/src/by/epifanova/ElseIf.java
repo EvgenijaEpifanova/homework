11486946cb41036a475c0b4cffe197ac6ac51359
EvgenijaEpifanova <evgen.yakovleva.forever@gmail.com>
2020-02-23 18:17:39 +0300

    first commit

---
+++ homework22.02/src/by/epifanova/ElseIf.java
@@ -0,0 +1,12 @@
+package by.epifanova;
+
+class Main {
+    public static void main(String[] args) {
+        int x = 476327;
+        int b = x % 10;
+        if (b==7)
+            System.out.println("семёрочка");
+        else
+            System.out.println("что-то несемёрочка");
+    }
+}
