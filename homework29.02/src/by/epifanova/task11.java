b79f7807c5968bf563706724d4589c1c58abd8b6
EvgenijaEpifanova <evgen.yakovleva.forever@gmail.com>
2020-03-03 18:13:58 +0300

    first commit

--- homework29.02/src/by/epifanova/task11.java
+++ homework29.02/src/by/epifanova/task11.java
@@ -1,4 +1,14 @@
 package by.epifanova;
 
 public class task11 {
+    public static void main(String[] args) {
+
+        int i = 6;
+        if ((i % 3 == 0) & (i % 1 == 0)) {
+            System.out.println("простое");
+        } else {
+            System.out.println("непростое");
+        }
+    }
 }
+
---
+++ task8-10/src/by/epifanova/task9.java
@@ -0,0 +1,11 @@
+package by.epifanova;
+
+public class task9 {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
