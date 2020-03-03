e77e7a58f6497c624b73cebf4cf4068c195a7cec
EvgenijaEpifanova <evgen.yakovleva.forever@gmail.com>
2020-03-03 18:34:26 +0300

    commit

--- homework29.02/src/by/epifanova/task12.java
+++ homework29.02/src/by/epifanova/task12.java
@@ -1,4 +1,14 @@
 package by.epifanova;
 
 public class task12 {
+    public static void main(String[] args) {
+        int i = 20623143;
+        int x1 = i%1000;
+        int x2;
+        x2 = (i/1000) %1000;
+        int x3;
+        x3= (i/1000000) %1000;
+        System.out.println(x3 + " " + x2 + " " + x1);
+
+    }
 }
