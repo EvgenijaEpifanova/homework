4190f17b20004c9fdb58b3c7859629d9902419d6
EvgenijaEpifanova <evgen.yakovleva.forever@gmail.com>
2020-04-05 12:49:08 +0300

    Fixing error in Task2-4

--- homework22.02/src/by/epifanova/task4.java
+++ homework22.02/src/by/epifanova/task4.java
@@ -2,7 +2,7 @@ package by.epifanova;
 
 import java.util.Scanner;
 
-public class Opening {
+public class task4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите длинну прямоугльника: ");
